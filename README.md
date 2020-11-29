dd59af7a32a1d9f202b217742489c4846235d8b6
Mamba-11 <70580918+Mamba-11@users.noreply.github.com>
2020-11-29 16:30:40 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Cars-and-Walls
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,52 @@
+var bullet;
+var wall;
+var speed,weight,thickness;
+
+function setup() {
+  createCanvas(1600,400);
+  bullet = createSprite(200, 200, 40, 40);
+  speed = random(223,321);
+  bullet.velocityX = speed;
+
+  weight= random(30,52);
+  thickness = random(22,83)
+  wall = createSprite(1200,200,thickness,height/2);
+}
+
+function hasCollided(lbullet,lwall){
+  bulletRightEdge=lbullet.x +lbullet.width;
+  wallLeftEdge = lwall.x;
+  
+  if(bulletRightEdge>=wallLeftEdge){
+    return true
+  }
+
+  return false;
+}
+
+
+
+
+
+function draw() {
+  background(255,255,255);  
+ 
+  if(hasCollided(bullet,wall)){
+
+    bullet.velocityX = 0;
+    var damage=0.5 * weight*speed* speed/(thickness * thickness * thickness);
+  }
+  
+  if(damage>10)
+  {
+    wall.shapecolor = color(255,0,0);
+  }
+  
+  
+  if(damage<10){
+    wall.shapeColor=color(0,255,0);
+  }
+  
+ drawSprites();
+}
+
